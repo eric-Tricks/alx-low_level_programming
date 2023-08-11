1719e293fa50409055c6f8a7be515dc3bcb57e22
eric-Tricks <ericamukowa@gmail.com>
2023-08-11 05:50:33 -0700

    Updated files

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else if (last_digit == 0)
 	{
-		printf("The last string of %d is %d and is 0\n", n, last_digit);
+		printf("Last string of %d is %d and is 0\n", n, last_digit);
 	}
 	else if (last_digit < 6 && last_digit != 0)
 	{
