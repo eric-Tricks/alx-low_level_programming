b36703c637d9d80b066c65283442afd41ab3236b
eric-Tricks <ericamukowa@gmail.com>
2023-08-17 21:35:47 -0700

    Updated files

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints line on termnal based on n value
+ *@n: the number of __ to print
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int num;
+
+	if (n > 0)
+	{
+		for (num = 0; num <= n; num++)
+			_putchar('_');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
